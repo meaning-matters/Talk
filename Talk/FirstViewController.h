a84f91738fca3c87741e0d831a6d1811bf100e93
case <k.vanderbent@mondialtelecom.be>
2012-09-28 14:24:50 +0200

    Initial Commit

---
+++ Talk/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  Talk
+//
+//  Created by Cornelis van der Bent on 28/09/12.
+//  Copyright (c) 2012 Cornelis van der Bent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
---
+++ TalkTests/TalkTests.m
@@ -0,0 +1,32 @@
+//
+//  TalkTests.m
+//  TalkTests
+//
+//  Created by Cornelis van der Bent on 28/09/12.
+//  Copyright (c) 2012 Cornelis van der Bent. All rights reserved.
+//
+
+#import "TalkTests.h"
+
+@implementation TalkTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in TalkTests");
+}
+
+@end
