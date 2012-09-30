5cee15d1b3a5f7b84499224524621ecab4aabe59
case <cg_vanderbent@mail.com>
2012-09-30 22:25:00 +0200

    Removed useless pjsua code.

---
+++ Talk/Sources/Common.h
@@ -0,0 +1,15 @@
+//
+//  Common.h
+//  Talk
+//
+//  Created by Cornelis van der Bent on 30/09/12.
+//  Copyright (c) 2012 Cornelis van der Bent. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Common : NSObject
+
++ (NSString*)documentFilePath:(NSString*)fileName;
+
+@end
