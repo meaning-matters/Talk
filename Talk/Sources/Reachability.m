23d29115d8601bb92bd1439c00dd9944789fbcaf
case <cg_vanderbent@mail.com>
2012-11-09 01:15:24 +0100

    Some more changes to NetworkStatus.

--- Talk/Sources/Reachability.m
+++ Talk/Sources/Reachability.m
@@ -1,29 +1,10 @@
-/*
- Copyright (c) 2011, Tony Million.
- All rights reserved.
- 
- Redistribution and use in source and binary forms, with or without
- modification, are permitted provided that the following conditions are met:
- 
- 1. Redistributions of source code must retain the above copyright notice, this
- list of conditions and the following disclaimer.
- 
- 2. Redistributions in binary form must reproduce the above copyright notice,
- this list of conditions and the following disclaimer in the documentation
- and/or other materials provided with the distribution.
- 
- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
- LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- POSSIBILITY OF SUCH DAMAGE. 
- */
+//
+//  Reachability.m
+//  Talk
+//
+//  Created by Cornelis van der Bent on 08/11/12.
+//  Copyright (c) 2011 Tony Million.
+//  Copyright (c) 2012 Cornelis van der Bent. All rights reserved.
 
 #import "Reachability.h"
 
@@ -187,13 +168,13 @@ static void TMReachabilityCallback(SCNetworkReachabilityRef   target,
     
     // First we need to create a serial queue.  We allocate this once for the
     // lifetime of the notifier
-    self.reachabilitySerialQueue = dispatch_queue_create("com.tonymillion.reachability", NULL);
+    self.reachabilitySerialQueue = dispatch_queue_create("reachability", NULL);
     if(!self.reachabilitySerialQueue)
     {
         return NO;
     }
     
-    context.info = (__bridge void *)self;
+    context.info = (__bridge void*)self;
     
     if (!SCNetworkReachabilitySetCallback(self.reachabilityRef, TMReachabilityCallback, &context)) 
     {
@@ -357,7 +338,7 @@ static void TMReachabilityCallback(SCNetworkReachabilityRef   target,
 
 // WWAN may be available, but not active until a connection has been established.
 // WiFi may require a connection for VPN on Demand.
--(BOOL)isConnectionRequired
+- (BOOL)isConnectionRequired
 {
     SCNetworkReachabilityFlags flags;
 	
