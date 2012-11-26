7133f80a3bbb2382b85c80aaa29250835cf65fdb
case <k.vanderbent@mondialtelecom.be>
2012-11-26 17:29:45 +0100

    Workaround AVAudioPlayer delay that kicks in after 5 seconds not playing anything.

--- Talk/Sources/DtmfPlayer.m
+++ Talk/Sources/DtmfPlayer.m
@@ -5,14 +5,28 @@
 //  Created by Cornelis van der Bent on 24/11/12.
 //  Copyright (c) 2012 Cornelis van der Bent. All rights reserved.
 //
+//  The keep-alive works around a weird play delay that kicks in after about 5 seconds
+//  of not having played anything.  By playing something silently, the delay is prevented.
+//  Other people have seen similar delays, and the solution given is to use lower
+//  level APIs.  I don't want to spend more time on this, and choose work around.
 
 #import <AVFoundation/AVAudioPlayer.h>
 #import "DtmfPlayer.h"
 
-static NSMutableDictionary* audioDataObjects;
+
+@interface DtmfPlayer ()
+{
+    NSMutableDictionary*    audioDataObjects;
+    NSTimer*                keepAliveTimer;
+    AVAudioPlayer*          audioPlayer;    // Needed; can't use local, otherwise ARC releases it immediately.
+}
+
+@end
 
 
 @implementation DtmfPlayer
+
+static DtmfPlayer*  sharedPlayer;
 
 
 + (BOOL)initializeSound:(char)character name:(NSString*)name
@@ -23,7 +37,7 @@ static NSMutableDictionary* audioDataObjects;
 
     if (error == nil && data != nil)
     {
-        [audioDataObjects setObject:data forKey:[NSString stringWithFormat:@"%c", character]];
+        [sharedPlayer->audioDataObjects setObject:data forKey:[NSString stringWithFormat:@"%c", character]];
 
         return YES;
     }
@@ -36,28 +50,89 @@ static NSMutableDictionary* audioDataObjects;
 
 + (void)initialize
 {
-    audioDataObjects = [NSMutableDictionary dictionary];
-
-    for (char character = '0'; character <= '9'; character++)
+    if ([DtmfPlayer class] == self)
     {
-        if ([DtmfPlayer initializeSound:character name:[NSString stringWithFormat:@"%c", character]] == NO)
+        sharedPlayer = [self new];
+
+        sharedPlayer->audioDataObjects = [NSMutableDictionary dictionary];
+
+        for (char character = '0'; character <= '9'; character++)
+        {
+            if ([DtmfPlayer initializeSound:character name:[NSString stringWithFormat:@"%c", character]] == NO)
+            {
+                NSLog(@"Error loading DTMF sound.");
+                break;
+            }
+        }
+
+        if ([DtmfPlayer initializeSound:'*' name:@"star"] == NO ||
+            [DtmfPlayer initializeSound:'#' name:@"pound"]== NO)
         {
             NSLog(@"Error loading DTMF sound.");
-            break;
         }
-    }
-
-    if ([DtmfPlayer initializeSound:'*' name:@"star"] == NO ||
-        [DtmfPlayer initializeSound:'#' name:@"pound"]== NO)
-    {
-        NSLog(@"Error loading DTMF sound.");
     }
 }
 
 
-+ (void)playForCharacter:(char)character
++ (id)allocWithZone:(NSZone*)zone
 {
-    static AVAudioPlayer*   audioPlayer;    // Needs to be static, otherwise ARC releases it immediately.
+    if (sharedPlayer && [DtmfPlayer class] == self)
+    {
+        [NSException raise:NSGenericException format:@"Duplicate DtmfPlayer singleton creation"];
+    }
+
+    return [super allocWithZone:zone];
+}
+
+
++ (DtmfPlayer*)sharedPlayer
+{
+    return sharedPlayer;
+}
+
+
+- (void)startKeepAlive
+{
+    if (keepAliveTimer == nil)
+    {
+        keepAliveTimer = [NSTimer scheduledTimerWithTimeInterval:4
+                                                          target:sharedPlayer
+                                                        selector:@selector(playKeepAlive)
+                                                        userInfo:nil
+                                                         repeats:YES];
+    }
+}
+
+
+- (void)stopKeepAlive
+{
+    [keepAliveTimer invalidate];
+    keepAliveTimer = nil;
+}
+
+
+- (void)playKeepAlive
+{
+    if ([audioPlayer isPlaying] == NO)
+    {
+        NSData*                 data;
+        NSError*                error = nil;
+
+        data = [audioDataObjects objectForKey:[NSString stringWithFormat:@"%c", '0']];
+        audioPlayer = [[AVAudioPlayer alloc] initWithData:data error:&error];
+        audioPlayer.volume= 0.00001f;
+        [audioPlayer prepareToPlay];
+
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^
+                       {
+                           [audioPlayer play];
+                       });
+    }
+}
+
+
+- (void)playForCharacter:(char)character
+{
     NSData*                 data;
     NSError*                error = nil;
 
@@ -66,7 +141,10 @@ static NSMutableDictionary* audioDataObjects;
     audioPlayer.volume= 0.02f;
     [audioPlayer prepareToPlay];
 
-    [audioPlayer play];
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^
+                   {
+                       [audioPlayer play];
+                   });
 }
 
 @end
