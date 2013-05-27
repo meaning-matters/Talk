10c4795d1650514767ecab601b0562b92dfdc0b8
case <cg_vanderbent@mail.com>
2013-05-27 17:24:34 +0200

    Added black & white ShareButton images.

--- Talk/Sources/ShareButton.m
+++ Talk/Sources/ShareButton.m
@@ -10,16 +10,6 @@
 
 @implementation ShareButton
 
-- (id)initWithFrame:(CGRect)frame
-{
-    if (self = [super initWithFrame:frame])
-    {
-    }
-    
-    return self;
-}
-
-
 - (void)setHighlighted:(BOOL)highlighted
 {
     if (self.isHighlighted != highlighted)
@@ -35,17 +25,25 @@
 - (void)drawRect:(CGRect)rect
 {
     //// Color Declarations
-    UIColor* fillColor   = self.highlighted ? self.color : [UIColor colorWithWhite:0.5f alpha:1.0f];
-    UIColor* strokeColor = [UIColor colorWithRed: 0 green: 0 blue: 0 alpha: 0.8];
+    UIColor*    fillColor;
+    if (self.color != nil)
+    {
+        fillColor = self.highlighted ? self.color : [UIColor whiteColor];
+    }
+    else
+    {
+        fillColor = self.highlighted ? [UIColor colorWithWhite:0.6f alpha:1.0f] : [UIColor whiteColor];
+    }
+    
+    UIColor* strokeColor = [UIColor colorWithRed:0 green:0 blue:0 alpha:0.8];
 
     //// Rounded Rectangle Drawing
-    UIBezierPath* roundedRectanglePath = [UIBezierPath bezierPathWithRoundedRect: CGRectMake(0.5, 0.5, 79, 79) cornerRadius: 12];
+    UIBezierPath* roundedRectanglePath = [UIBezierPath bezierPathWithRoundedRect: CGRectMake(0.5, 0.5, 79, 79) cornerRadius:12];
     [fillColor setFill];
     [roundedRectanglePath fill];
     [strokeColor setStroke];
     roundedRectanglePath.lineWidth = 1;
     [roundedRectanglePath stroke];
-
 }
 
 @end
