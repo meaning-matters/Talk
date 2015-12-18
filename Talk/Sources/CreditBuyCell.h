29183789b9ed4ee5eaa2cfbaabb830cb647810e8
Cornelis van der Bent <cornelis@meaning-matters.com>
2015-12-18 07:13:22 +0100

    Removed Credit60 and 72 UI.

--- Talk/Sources/CreditBuyCell.h
+++ Talk/Sources/CreditBuyCell.h
@@ -24,16 +24,12 @@
 @property (nonatomic, weak) IBOutlet UIButton*                button10;
 @property (nonatomic, weak) IBOutlet UIButton*                button20;
 @property (nonatomic, weak) IBOutlet UIButton*                button50;
-@property (nonatomic, weak) IBOutlet UIButton*                button100;
-@property (nonatomic, weak) IBOutlet UIButton*                button200;
 @property (nonatomic, weak) IBOutlet UIActivityIndicatorView* activityIndicator1;
 @property (nonatomic, weak) IBOutlet UIActivityIndicatorView* activityIndicator2;
 @property (nonatomic, weak) IBOutlet UIActivityIndicatorView* activityIndicator5;
 @property (nonatomic, weak) IBOutlet UIActivityIndicatorView* activityIndicator10;
 @property (nonatomic, weak) IBOutlet UIActivityIndicatorView* activityIndicator20;
 @property (nonatomic, weak) IBOutlet UIActivityIndicatorView* activityIndicator50;
-@property (nonatomic, weak) IBOutlet UIActivityIndicatorView* activityIndicator100;
-@property (nonatomic, weak) IBOutlet UIActivityIndicatorView* activityIndicator200;
 
 @property (nonatomic, weak) id<CreditBuyCellDelegate>         delegate;
 
