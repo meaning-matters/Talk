df0b2cae2e9ca187aec46f79589997e43029c7d1
Cornelis van der Bent <cornelis@meaning-matters.com>
2016-03-26 22:04:00 +0100

    Fix crash 'Collection was mutated while being enumerated'.

--- Talk/Sources/WebClient.m
+++ Talk/Sources/WebClient.m
@@ -145,7 +145,7 @@
 {
     if ([object isKindOfClass:[NSMutableDictionary class]])
     {
-        for (NSString* key in object)
+        for (NSString* key in [object allKeys])
         {
             if ([key isEqualToString:@"e164s"])
             {
