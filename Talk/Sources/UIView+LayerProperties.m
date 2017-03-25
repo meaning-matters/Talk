bb47b19c8a53b2957f7d02ee7f086c2902412263
Cornelis van der Bent <cornelis@meaning-matters.com>
2017-03-25 15:16:21 +0100

    Code cosmetics.

--- Talk/Sources/UIView+LayerProperties.m
+++ Talk/Sources/UIView+LayerProperties.m
@@ -12,49 +12,49 @@
 IB_DESIGNABLE
 @implementation UIView (LayerProperties)
 
-- (CGFloat) borderWidth;
+- (CGFloat)borderWidth;
 {
     return self.layer.borderWidth;
 }
 
 
-- (void) setBorderWidth: (CGFloat) borderWidth;
+- (void)setBorderWidth:(CGFloat)borderWidth;
 {
     self.layer.borderWidth = borderWidth;
 }
 
 
-- (void) setBorderColor:(UIColor *)borderColor
+- (void)setBorderColor:(UIColor*)borderColor
 {
     self.layer.borderColor = borderColor.CGColor;
 }
 
 
-- (UIColor *) borderColor;
+- (UIColor*)borderColor;
 {
     return [UIColor colorWithCGColor: self.layer.borderColor];
 }
 
 
-- (CGFloat) cornerRadius;
+- (CGFloat)cornerRadius;
 {
     return self.layer.cornerRadius;
 }
 
 
-- (void) setCornerRadius: (CGFloat) cornerRadius;
+- (void)setCornerRadius:(CGFloat)cornerRadius;
 {
     self.layer.cornerRadius = cornerRadius;
 }
 
 
-- (void) setLayerBackgroundColor:(UIColor *)layerBackgroundColor
+- (void)setLayerBackgroundColor:(UIColor*)layerBackgroundColor
 {
     self.layer.backgroundColor = layerBackgroundColor.CGColor;
 }
 
 
-- (UIColor *) layerBackgroundColor;
+- (UIColor*)layerBackgroundColor;
 {
     return [UIColor colorWithCGColor: self.layer.backgroundColor];
 }
