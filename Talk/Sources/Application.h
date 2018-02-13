4c0a752272f727e340ec366e2c2e222b22361918
Cornelis van der Bent <cornelis@meaning-matters.com>
2018-02-13 09:05:57 +0100

    Set preferredContentSizeCategory to ...ExtraLarge making sure font sizes in app are fixed

---
+++ Talk/Sources/Application.h
@@ -0,0 +1,13 @@
+//
+//  Application.h
+//  Talk
+//
+//  Created by Cornelis van der Bent on 28/01/2018.
+//  Copyright © 2018 NumberBay Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Application: UIApplication
+
+@end
